4da8e2d6f56f8c5709105031095411a463c39761
Stefan Bogdan Nyari <StefanBN@users.noreply.github.com>
2016-06-22 20:56:05 +0200

    old quota check script
    This script assumes that the user will input the arguments in a specific order: quota_check.sh filer volume or quota_check.sh filer vfiler volume

---
+++ bash/quota_check.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+## script to get quota sum
+
+if [ $# -lt 2 ]; then
+  echo "$0 filer volume"
+  echo "or"
+  echo "$0 filer vfiler volume"
+fi
+
+if [ $# -eq 2 ]; then
+  filer=$1
+  volume=$2
+  echo "Checking quota for volume: $2"
+  echo -e "\tSpace available in AFS: \c"
+  ssh -n -l root $filer df -h $volume | egrep -v "Filesystem|snap" | awk '{print $2}'
+  echo -e "\tTotal quota size: \c"
+  ssh -n -l root $filer quota report | grep $volume | awk 'BEGIN{sum=0} {sum=sum+$6} END{print sum}' | kb2gb
+else
+  if [ $# -eq 3 ]; then
+    filer=$1
+    vfiler=$2
+    volume=$3
+    echo "Checking quota for volume: $3"
+    echo -e "\tSpace available in AFS: \c"
+    ssh -n -l root $filer df -h $volume | egrep -v "Filesystem|snap" | awk '{print $2}'
+    echo -e "\tTotal quota size: \c"
+    ssh -n -l root $filer vfiler run $vfiler quota report | grep $volume | awk 'BEGIN{sum=0} {sum=sum+$6} END{print sum}' | kb2gb
+  fi
+fi
